4d5e75853945b20c1dbc58197d7e7b618df36285
Ayesha sadaf <ayeshasadaf299@gmail.com>
2025-12-09 21:36:01 +0500

    Created List and Detailed Views for books and authors

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView,DetailView
 from .models import Author,Book,BookInstance,Genre
 
+#Function based view
 def index(request):
     'View funcrion for home page of the site'
 
@@ -23,12 +25,23 @@ def index(request):
         "num_books_self_help":num_books_self_help
 
     }
-
     return render(request,'index.html',context=context)
 
 
+class BookListView(ListView):
+    model = Book
 
+    # def getquery_set(self):
+    #     return Book.objects.filter(title__icontains='murder')[:5]
+   
+class BookDetailView(DetailView):
+    model=Book
 
+class AuthorListView(ListView):
+    model=Author
+    
+class AuthorDetailView(DetailView):
+    model=Author
 
 
 
