53262a8031e2461b6d701b72b963ee70f3d9fae6
Ayesha Sadaf <124766781+Ayesha-sadaf@users.noreply.github.com>
2025-12-24 21:27:56 +0500

    Change email credentials to placeholders
    Updated email settings to use placeholder values.

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ayeshasadaf299@gmail.com'
-EMAIL_HOST_PASSWORD = 'yier nzsf zhqu fltc'
+EMAIL_HOST_USER = '<youremail>@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
-DEFAULT_FROM_EMAIL = 'Library Admin <ayeshasadaf299@gmail.com>'
+DEFAULT_FROM_EMAIL = 'Library Admin <@gmail.com>'
